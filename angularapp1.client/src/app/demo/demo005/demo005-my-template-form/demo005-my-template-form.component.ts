515c0aaba64db08d3d58928fd3b11f72385a44ee
Rely Ky <relyky@hotmail.com>
2024-09-25 15:42:47 +0800

    表單 - Template Form

--- angularapp1.client/src/app/demo/demo005/demo005-my-template-form/demo005-my-template-form.component.ts
+++ angularapp1.client/src/app/demo/demo005/demo005-my-template-form/demo005-my-template-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-demo005-my-template-form',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './demo005-my-template-form.component.css'
 })
 export class Demo005MyTemplateFormComponent {
+  // 表單輸入
+  myFormData?: object = undefined;
 
+  handleMyFormSubmit(myFrom: NgForm) {
+    console.log('handleMyFormSubmit', { myFrom });
+    this.myFormData = {
+      fieldA: myFrom.value['fieldA'],
+      fieldB: myFrom.value['fieldB'],
+    };
+  }
 }
