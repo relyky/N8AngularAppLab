637d5b11a77a8385013ecd9b7f548dfe529ac79a
Rely Ky <relyky@hotmail.com>
2024-09-25 16:57:29 +0800

    表單 - Reactive Form

--- angularapp1.client/src/app/app.module.ts
+++ angularapp1.client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -42,6 +42,7 @@ import { Demo005MyTemplateFormComponent } from './demo/demo005/demo005-my-templa
     HttpClientModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
--- angularapp1.client/src/app/demo/demo005/demo005-my-reactive-form/demo005-my-reactive-form.component.ts
+++ angularapp1.client/src/app/demo/demo005/demo005-my-reactive-form/demo005-my-reactive-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-demo005-my-reactive-form',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrl: './demo005-my-reactive-form.component.css'
 })
 export class Demo005MyReactiveFormComponent {
+  myForm!: FormGroup; // 控制整個表單
+  myField: FormControl = new FormControl('郝聰明', Validators.required); // 控制單欄位
+
+  ngOnInit() {
+    this.myForm = new FormGroup({
+      name: new FormControl('郝聰明', Validators.required),
+      email: new FormControl('smart@mail.server', [Validators.required, Validators.email]),
+      salary: new FormControl(16888, [Validators.required, Validators.min(1000)]),
+    });
+  }
+
+  handleSubmit() {
+    if (this.myForm.valid) {
+      console.info('handleSubmit', { formData: this.myForm.value, form: this.myForm });
+    }
+  }
+
+  handleReset() {
+    this.myForm.reset({
+      name: '預設名稱',
+      email: 'reset@mail.server',
+      salary: 900
+    });
+  }
+
+  handleWatch() {
+    console.info('handleWatch', { formData: this.myForm.value, form: this.myForm });
+  }
+
 
 }
